a400d49867936d19ef90704fb0f0aaf4a2f54440
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-04-09 09:44:10 +0800

    Add a queries file that handles the query for the the app

--- db/queries.js
+++ db/queries.js
@@ -1,1 +1,67 @@
 const { prisma } = require("./prisma");
+
+class Folder {
+
+  async addFolder(userId, folderName) {
+    await prisma.folder.create({
+      data: {
+        name: folderName,
+        author: {
+          connect: {
+            id: userId
+          }
+        }
+      }
+    });
+  }
+
+  async allFolder(userId) {
+    const folders = await prisma.folder.findMany({
+      where: {
+        AND: [
+        { authorId: userId },
+        { parentFolderId: null }          
+        ]
+      }
+    });
+    return folders;
+  }
+
+  async toFolderPage(id) {
+    const folder = await prisma.folder.findUnique({
+      where: {
+        id: id
+      }
+    });
+
+    return folder
+  }
+
+  async addChildFolder(folderId, name, userId) {
+    await prisma.folder.create({
+      data: {
+        name: name,
+        parentFolderId: folderId,
+        authorId: userId
+      }
+    });
+  }
+
+  async childFolder(Id) {
+    const childFolder = await prisma.folder.findUnique({
+      where: {id: Id},
+      include: {
+        childFolder: true
+      }
+    });
+
+    return childFolder;
+  }
+
+}
+
+const folderMethod = new Folder();
+
+module.exports = {
+  folderMethod
+}
