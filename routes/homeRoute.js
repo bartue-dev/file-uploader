be9455c016013fd6d387ed7a2bf650b79c6c1808
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-04-11 20:43:40 +0800

    import multer library that allows us to post files initialize the necessary code to post a single file in a memory then upload it to cloud storage supabase. Import the postFolderAndFile and postFile homeController

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,15 +1,32 @@
 const { Router } = require("express")
+const multer = require("multer");
 const homeRoute = Router();
 const homeController = require("../controllers/homeController");
 
+//store uploaded files in memory
+const storage = multer.memoryStorage();
 
+//initialize multer with the storage configuration
+const upload = multer({ storage: storage });
+
+//
 homeRoute.get("/", homeController.getHome);
 
+//
 homeRoute.post("/add-folder", homeController.postCreateFolder);
 
+//
 homeRoute.get("/:id/folder", homeController.getHome);
 
+//
 homeRoute.post("/:id/child-folder", homeController.postChildFolder);
+
+//
+homeRoute.post("/:id/upload-file", upload.single("file"), homeController.postFileInFolder);
+
+//
+homeRoute.post("/upload-file", upload.single("file"), homeController.postFile);
+
 
 module.exports = homeRoute;
 
