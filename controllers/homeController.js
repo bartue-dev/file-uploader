5518c9f5004e5912f9753e88b7dd57bea67aed92
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-04-11 20:36:28 +0800

    Add a postFile controller and postFileInFolder controler. Used supabase as a cloud storage then get the url of the file from it then store it into my local database

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,46 +1,146 @@
 const asyncHandler = require("express-async-handler");
+const { supabase } = require("../storage/supabase");
+const { decode } = require("base64-arraybuffer");
 const db = require("../db/queries");
 
-
+//render home page along with the datas
 exports.getHome = asyncHandler(async (req, res, next) => {
   const { id } = req.user;
   const paramsId = req.params.id
   
-  const folders = await db.folderMethod.allFolder(id)
-  let childFolder;
+  const mainFolderAndFile = await db.folderMethod.mainFolderAndFile(id)
   let folder;
   
   if (paramsId) {
     folder = await db.folderMethod.toFolderPage(Number(paramsId));
-    childFolder = await db.folderMethod.childFolder(Number(paramsId));
   }
 
-  console.log("folder: ", folder);
+  console.log("FOLDER:", folder)
+  // console.log("FOLDERS&FILES:", mainFolderAndFile[0].file);
   
 
   res.render("home", {
     title: "Home",
-    folders: folders || [],
-    folder: folder || [],
-    childFolder: childFolder || []
+    mainFolderAndFile: mainFolderAndFile[0] || [],
+    folder: folder,
   });
 });
 
+//create folder
 exports.postCreateFolder = asyncHandler(async (req, res, next) => {
   const { folderName } = req.body;
-  const { id } = req.user;
+  const userId = req.user.id;
 
-  await db.folderMethod.addFolder(id, folderName);
+  await db.folderMethod.addFolder(userId, folderName);
 
-  res.redirect("/home")
+  res.redirect("/home");
 
 });
 
+//create child folder
 exports.postChildFolder = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
   const { folderName } = req.body
+  const userId = req.user.id;
 
-  await db.folderMethod.addChildFolder(Number(id), folderName);
+  await db.folderMethod.addChildFolder(Number(id), folderName, userId);
 
   res.redirect(`/home/${id}/folder`)
+});
+
+//create file
+exports.postFileInFolder = asyncHandler(async (req, res, next) => {
+  const {originalname, buffer, mimetype} = req.file;
+  const userId = req.user.id;
+  const folderId = Number(req.params.id); 
+
+  if (!req.file) {
+    res.status(400).json({message: "Please upload file"})
+  }
+
+  const filePath = `${userId}/${folderId}/${originalname}`;
+  const fileBase64 = decode(buffer.toString("base64"));
+
+  const { data, error } = await supabase.storage
+  .from("files")
+  .upload(filePath, fileBase64, {
+    contentType: mimetype
+  });
+
+  // if (error) {
+  //   console.error("Supabase upload error:", error);
+  //   return res.status(500).json({
+  //     message: "Failed to upload file to Supabase",
+  //     error: error
+  //   });
+  // }
+
+  // console.log("Upload successful:", data);
+
+  const { data: urlData } = supabase.storage
+    .from("files")
+    .getPublicUrl(filePath)
+
+  const publicUrl = urlData.publicUrl;
+
+  await db.fileMethod.addFile({
+    authorId: userId,
+    folderId: folderId,
+    name: originalname,
+    url: publicUrl,
+    type: mimetype 
+  });
+
+
+  // console.log("File url", publicUrl)
+
+  res.redirect(`/home/${folderId}/folder`);
+});
+
+//upload file without a folder
+exports.postFile = asyncHandler(async (req, res, next) => {
+  const { originalname, buffer, mimetype} = req.file;
+  const userId = req.user.id;
+
+ if (!req.file) {
+    res.status(400).json({message: "Please upload file"})
+  }
+
+  const filePath = `${userId}/${originalname}`;
+  const fileBase64 = decode(buffer.toString("base64"));
+
+  const { data, error } = await supabase.storage
+  .from("files")
+  .upload(filePath, fileBase64, {
+    contentType: mimetype
+  });
+
+  // if (error) {
+  //   console.error("Supabase upload error:", error);
+  //   return res.status(500).json({
+  //     message: "Failed to upload file to Supabase",
+  //     error: error
+  //   });
+  // }
+
+  // console.log("Upload successful:", data);
+
+
+  const { data: urlData } = supabase.storage
+    .from("files")
+    .getPublicUrl(filePath)
+
+  const publicUrl = urlData.publicUrl;
+
+  await db.fileMethod.addFile({
+    authorId: userId,
+    name: originalname,
+    url: publicUrl,
+    type: mimetype,
+    authorId: userId
+  });
+
+  // console.log("File url", publicUrl)
+
+  res.redirect("/home");
 });
