ab9ce0c194f77944e117f1a844c4bcd9f0b7c77c
Bartue <[bartbatiancila28@gmail.com]>
2025-04-14 19:23:17 +0800

    Import the deleteFile controller to have a post request that allows user to delete a specific file using the :id route as params given in client

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -9,23 +9,26 @@ const storage = multer.memoryStorage();
 //initialize multer with the storage configuration
 const upload = multer({ storage: storage });
 
-//
+//render home page
 homeRoute.get("/", homeController.getHome);
 
-//
-homeRoute.post("/add-folder", homeController.postCreateFolder);
-
-//
+//render specific folder in home page
 homeRoute.get("/:id/folder", homeController.getHome);
 
-//
+//add folder
+homeRoute.post("/add-folder", homeController.postCreateFolder);
+
+//add child folder
 homeRoute.post("/:id/child-folder", homeController.postChildFolder);
 
-//
+//upload a file with parent folder
 homeRoute.post("/:id/upload-file", upload.single("file"), homeController.postFileInFolder);
 
-//
+//upload file with no parent folder
 homeRoute.post("/upload-file", upload.single("file"), homeController.postFile);
+
+//delete file
+homeRoute.post("/:id/delete-file", homeController.deleteFile);
 
 
 module.exports = homeRoute;
