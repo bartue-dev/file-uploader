060c3b9050402cae37bea53fae9180b6d53cac40
Bartue <[bartbatiancila28@gmail.com]>
2025-04-15 20:41:53 +0800

    Import the validation from express-validator file. Input the validation in middleware that needs validation. If error is truthy display the error msg in the sign-up page

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,16 +1,28 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
 const { prisma } = require("../db/prisma");
+const { validationResult } = require("express-validator");
+const { validateSignUp } = require("../validator/express-validator")
 
 exports.getSignup = asyncHandler(async (req, res, next) => {
   res.render("sign-up", {
-    title: "Sign up"
+    title: "Sign up",
+    errors: []
   });
 });
 
-exports.postSignup = asyncHandler(async (req, res, next) => {
+exports.postSignup = [ validateSignUp, asyncHandler(async (req, res, next) => {
+
+  const errors = validationResult(req);
   const { username } = req.body;
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+  if (!errors.isEmpty()) {
+    return res.render("sign-up", {
+      title: "Sign up",
+      errors: errors.array()
+    });
+  }
 
   await prisma.user.create({
     data: {
@@ -21,4 +33,4 @@ exports.postSignup = asyncHandler(async (req, res, next) => {
 
   res.redirect("/");
 
-});
+})];
