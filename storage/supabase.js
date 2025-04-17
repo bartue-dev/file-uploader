0ab6ca890e39d50c8c34fbb8ed25ba4beab93851
Bartue <[bartbatiancila28@gmail.com]>
2025-04-17 21:56:23 +0800

    Edit env invocation

--- storage/supabase.js
+++ storage/supabase.js
@@ -1,7 +1,7 @@
 const { createClient  } = require("@supabase/supabase-js");
 
-const supabaseUrl = process.env.SUPABASE_PROJECT_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   console.error("Missing Supabase credentials. Check your environment variables.");
