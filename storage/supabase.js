fdc5525752862c9c027340940e92866e7eff2598
Bartue <[bartbatiancila28@gmail.com]>
2025-04-17 22:15:54 +0800

    Add console.log for deployment debugging

--- storage/supabase.js
+++ storage/supabase.js
@@ -3,6 +3,9 @@ const { createClient  } = require("@supabase/supabase-js");
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
+console.log('SUPABASE_URL:', supabaseUrl);
+console.log('SUPABASE_KEY:', supabaseKey);
+
 if (!supabaseUrl || !supabaseKey) {
   console.error("Missing Supabase credentials. Check your environment variables.");
 }
